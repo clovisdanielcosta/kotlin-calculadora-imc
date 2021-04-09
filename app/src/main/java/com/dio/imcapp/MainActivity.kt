d4df2d0205b2b6878ccbf034e8783da55bd07f9c
Clovis Daniel Costa <clovisdanielcosta@gmail.com>
2021-04-09 11:00:29 -0300

    First commit

---
+++ app/src/main/java/com/dio/imcapp/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.dio.imcapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.w("Lifecycle", "CREATE - estou criando a tela")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.w("Lifecycle", "START - deixei a tela visível para você")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.w("Lifecycle", "RESUME - agora você pode interagir com a tela")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.w("Lifecycle", "PAUSE - perdeu o foco, você não pode mais interagir")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.w("Lifecycle", "STOP - a tela não está mais visível mais ainda existe")
+        finish()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.w("Lifecycle", "DESTROY - oh não! a tela foi destruída")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.w("Lifecycle", "RESTART - a tela está retornando o fico")
+    }
+
+}
