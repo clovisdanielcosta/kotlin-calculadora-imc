511ecdb73d3afdfade6f6662324d08b6c4f782af
Clovis Daniel Costa <clovisdanielcosta@gmail.com>
2021-04-11 18:22:52 -0300

    Created function calcularIMC and added event onClickListener in MainActivity.kt.

--- app/src/main/java/com/dio/imcapp/MainActivity.kt
+++ app/src/main/java/com/dio/imcapp/MainActivity.kt
@@ -1,46 +1,45 @@
 package com.dio.imcapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doAfterTextChanged
+import androidx.core.widget.doOnTextChanged
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_constraint)
-        Log.w("Lifecycle", "CREATE - estou criando a tela")
+        setContentView(R.layout.activity_main)
+        setListeners()
     }
 
-    override fun onStart() {
-        super.onStart()
-        Log.w("Lifecycle", "START - deixei a tela visível para você")
+    private fun setListeners() {
+
+        alturaEDT?.doAfterTextChanged { text ->
+            // Toast.makeText(this, text.toString(), Toast.LENGTH_SHORT).show()
+        }
+
+        pesoEDT?.doOnTextChanged { text, _, _, _ ->
+            // titleTXT?.text = text
+        }
+
+        calcularBTN?.setOnClickListener {
+            calcularIMC(pesoEDT.text.toString(), alturaEDT?.text.toString())
+        }
     }
 
-    override fun onResume() {
-        super.onResume()
-        Log.w("Lifecycle", "RESUME - agora você pode interagir com a tela")
+    @SuppressLint("SetTextI18n")
+    private fun calcularIMC(peso: String, altura: String) {
+        val pesoX = peso.toFloatOrNull()
+        val alturaY = altura.toFloatOrNull()
+
+        if (pesoX != null && alturaY != null) {
+            val imc = pesoX / (alturaY * alturaY)
+            titleTXT.text = "Seu IMC é: %.2f".format(imc)
+        }
     }
 
-    override fun onPause() {
-        super.onPause()
-        Log.w("Lifecycle", "PAUSE - perdeu o foco, você não pode mais interagir")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.w("Lifecycle", "STOP - a tela não está mais visível mais ainda existe")
-
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.w("Lifecycle", "DESTROY - oh não! a tela foi destruída")
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        Log.w("Lifecycle", "RESTART - a tela está retornando o fico")
-    }
 
 }
